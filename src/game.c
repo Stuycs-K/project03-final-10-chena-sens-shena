ee18f0aef1cce8726c10b7a7c24896d5ddb9038f
Amber Shen <ashen40@stuy.edu>
2024-01-05 15:41:50 -0500

    Workign check answer, doesnt work when put into loop function

--- src/game.c
+++ src/game.c
@@ -15,28 +15,40 @@ void userInput(char* returnString) {
     strcpy(returnString,line);
     printf("Read: %s\n",line); //this doesn't accurately test it, but alr tested prev in main
 }
-void loop() {
-    char read[1024];
-    while (1) { //keep askign for input, 1s delay b/t guesses
-        userInput(read);
-        sleep(1);
-    }
-}
 void convertLower(char* string) {
     for (int i = 0;string[i];i++) {
-        // printf("i: %d",i);
         string[i] = tolower(string[i]);
     }
 }
 int checkAnswer(char* guess, char* ans) {
+    printf("Checking answer now");
     convertLower(ans); //convert song title to all lowercase
-    return (strcmp(guess,ans)==0);  //same 
+    printf("Converted lower: %s\n",ans);
+    int answer = strcmp(guess,ans);  //same 
+    // printf("Compare result: %d\n",answer);
+    return answer;
+}
+void loop() {
+    char read[1024];
+    char* song;
+    while (1) { //keep askign for input, 1s delay b/t guesses
+        userInput(read);
+        song = "Placeholder"; //get song name from array later
+        // printf("Compare result: %d",checkAnswer(read,song));
+        // if (checkAnswer(read,song)==0) {
+        //     printf("That is correct!");
+        // }
+        sleep(1);
+    }
 }
 int main() {
     // char read[1024];
     // loop();
-    char string[256] ="Hi";
-    convertLower(string);
-    printf("%s\n",string);
+    char string[256] ="Blank Space";
+    char test[256] ="Test";
+    // convertLower(string);
+    // loop();
+    printf("checkanswer: %d\n",checkAnswer(string,test));
+    // printf("%s\n",string);
     // convertLower(hi);
 }
