1433bea9fc85b29cc038ae6238e84f10dcecb3ab
Shaurya Sen <shauryasen12@gmail.com>
2024-01-05 15:14:12 -0500

    add rand_int fn

--- src/utils.c
+++ src/utils.c
@@ -3,6 +3,23 @@
 #include <mpg123.h>
 #include <ao/ao.h>
 
+
+int rand_int() {
+    int file = open("/dev/random", O_RDONLY);
+    int n;
+    int random_int = read(file ,&n, 4);
+    if (random_int==-1) exit(1);
+    if(n<0) {
+        n*=-1;
+    }
+    return n;
+
+}
+
 void playSong(const char *filename) {}
 
-char * random_song(char** songs) {}
+char * random_song(char** songs) {
+
+    
+
+}
