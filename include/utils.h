d524b313e58fad6cc8d277d18915d38447a51d74
Shaurya Sen <shauryasen12@gmail.com>
2024-01-04 23:58:51 -0500

    add libraries and  key util function

---
+++ include/utils.h
@@ -0,0 +1,7 @@
+#ifndef UTILS_H
+#define UTILS_H
+
+
+void playMP3(const char *filename) {}
+
+#endif
--- src/utils.c
+++ src/utils.c
@@ -1,1 +1,6 @@
+#include <stdio.h>
 #include <stdlib.h>
+#include <mpg123.h>
+#include <ao/ao.h>
+
+void playMP3(const char *filename) {}
