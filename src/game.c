f00a9b643a39cdb6751750313b795df166233065
ambers7 <ambrshen@gmail.com>
2024-01-04 20:57:22 -0500

    Delay of 1s between guesses

--- src/game.c
+++ src/game.c
@@ -5,27 +5,24 @@
 //Get user input for guess, with 1s cooldown between guesses (no server)'''
 
 void userInput(char* returnString) {
-    //size of line is always 8 bcuz pointer
-    char line[1024];
-    printf("Guess: ");
-    fflush(stdout);
-    fflush(stdin);
-    // printf("Sizeof line: %d",sizeof(line));
-    fgets(line,sizeof(line),stdin);
-    // fflush(stdout);
-    // fflush(stdin);
-    strcpy(returnString,line);
-    // printf("Read: %s\n",line);
-    // return line;
-    //sleep(1);
+    while(1) {
+        char line[1024];
+        printf("Guess: ");
+        fflush(stdout);
+        fflush(stdin);
+        fgets(line,sizeof(line),stdin);
+        strcpy(returnString,line);
+        printf("Read: %s\n",line); //this doesn't accurately test it, but alr tested prev in main
+        sleep(1);
+    }
+    // sleep(1);
 }
 int main() {
-    // char line[1024];
     char read[1024];
-    while(1) {
-        // printf("SIZE OF LINE: %d",sizeof(line));
+    // while(1) {
         userInput(read);
-        printf("Read: %s\n",read);
-    }
+    //     printf("Read: %s\n",read);
+    //     sleep(1);
+    // }
 
 }
