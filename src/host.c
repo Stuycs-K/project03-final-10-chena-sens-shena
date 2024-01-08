c8e183c5cc57a6866656c81c90ea569d0bf9ae2b
Anthony Chen <50970130+achen318@users.noreply.github.com>
2024-01-08 08:02:23 -0500

    Handle client read/write

--- src/host.c
+++ src/host.c
@@ -15,12 +15,29 @@ void handle_new_client(int listen_socket, int *clients)
         }
 }
 
-void handle_client(int *clients)
+void handle_client(fd_set read_fds, int *clients)
 {
+    char buff[BUFFER_SIZE];
+
     for (int i = 0; i < MAX_CLIENTS; ++i)
     {
         int client_socket = clients[i];
-        printf("%d\n", client_socket);
+
+        if (FD_ISSET(client_socket, &read_fds))
+        {
+            int bytes = read(client_socket, buff, sizeof(buff));
+
+            if (bytes == 0)
+            {
+                close(client_socket);
+                clients[i] = 0;
+            }
+            else
+            {
+                buff[0] = 'B';
+                write(client_socket, buff, sizeof(buff));
+            }
+        }
     }
 }
 
@@ -31,19 +48,31 @@ int main()
     fd_set read_fds;
 
     int clients[MAX_CLIENTS];
-    char buff[BUFFER_SIZE];
 
     while (1)
     {
         FD_ZERO(&read_fds);
         FD_SET(listen_socket, &read_fds);
 
-        err(select(listen_socket + 1, &read_fds, NULL, NULL, NULL), "select error");
+        int max_socket = listen_socket;
+
+        for (int i = 0; i < MAX_CLIENTS; ++i)
+        {
+            int client_socket = clients[i];
+
+            if (client_socket > 0)
+                FD_SET(client_socket, &read_fds);
+
+            if (client_socket > max_socket)
+                max_socket = client_socket;
+        }
+
+        err(select(max_socket + 1, &read_fds, NULL, NULL, NULL), "select error");
 
         if (FD_ISSET(listen_socket, &read_fds))
             handle_new_client(listen_socket, clients);
-        else
-            handle_client(clients);
+
+        handle_client(read_fds, clients);
     }
 
     return 0;
