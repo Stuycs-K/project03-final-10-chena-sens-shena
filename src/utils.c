128e934f5ed26f1f916ad88c9daec9eac5f5aa37
Shaurya Sen <shauryasen12@gmail.com>
2024-01-10 15:01:48 -0500

    fix and uncomment util fns

--- src/utils.c
+++ src/utils.c
@@ -1,20 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <sys/fcntl.h>
 
 
 
-// int rand_int() {
-//     int file = open("/dev/random", O_RDONLY);
-//     int n;
-//     int random_int = read(file ,&n, 4);
-//     if (random_int==-1) exit(1);
-//     if(n<0) {
-//         n*=-1;
-//     }
-//     return n;
+int rand_int() {
+    int file = open("/dev/random", O_RDONLY);
+    int n;
+    int random_int = read(file ,&n, 4);
+    if (random_int==-1) exit(1);
+    if(n<0) {
+        n*=-1;
+    }
+    return n;
 
-// }
+}
 
 void playSong(char *filename) {
 
@@ -29,11 +30,11 @@ void playSong(char *filename) {
 
 }
 
-// char * random_song(char** songs) {
+char * random_song(char** songs) {
 
-//     int size = len(songs);
-//     int n =  (rand_int() % size ) ;
+    int size = len(songs);
+    int n =  (rand_int() % size ) ;
 
-//     return songs[n];
+    return songs[n];
 
-// }
+}
