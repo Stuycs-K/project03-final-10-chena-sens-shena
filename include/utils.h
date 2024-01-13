7c3d3cd4e40812e4fdd9fdc79db7e7fb9b8ff3d5
Shaurya Sen <shauryasen12@gmail.com>
2024-01-13 12:55:51 -0500

    fix utils.h

--- include/utils.h
+++ include/utils.h
@@ -9,26 +9,11 @@
 #define YELLOW "\e[33m"
 #define CLEAR "\e[0m"
 #define ERASE "\e[A\e[K"
-struct player
-{
-  int id;
-  char name[NAME_SIZE];
-  int points;
-};
 
-struct song
-{
-  char name[NAME_SIZE];
-  char file_name[NAME_SIZE]; //mp3 file name 
-};
-struct song random_song(struct song* songs, int total_songs, struct song* played_songs, int total_played_songs);
-int song_used(const char* songName, const struct song* played_songs, int total_played_songs);
 
-void play_song(char *filename) ;
-int awardPoints(struct player *playerlist, int n, int playerid, char *guess, char *ans);
-void leaderboard(struct player *playerlist, int n);
-int checkAnswer(char *guess, char *ans);
+
 void convertLower(char *string);
-void userInput(char *returnString);
+int rand_int();
+
 
 #endif
